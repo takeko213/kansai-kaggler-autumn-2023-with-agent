0879b1065217514442563ad060dbb3e552993bac
takeko213 <takehara213@gmail.com>
2023-10-18 16:37:34 +0000

    CityPlanningのCountEncoding特徴量を追加 #11

--- run.py
+++ run.py
@@ -75,6 +75,9 @@ df['Year-BuildingYear'] = df['Year'] - df['BuildingYear']
 # PrefectureのCountEncoding特徴量を追加
 df["Prefecture_count"] = df["Prefecture"].map(df["Prefecture"].value_counts())
 
+# CityPlanningのCountEncoding特徴量を追加
+df["CityPlanning_count"] = df["CityPlanning"].map(df["CityPlanning"].value_counts())
+
 target = "TradePrice"
 not_use_cols = [
     "row_id", "Prefecture", "Municipality", "DistrictName", "NearestStation",
