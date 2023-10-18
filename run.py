9c03d016af0f98edee971ce8b2ce014039972a57
takeko213 <takehara213@gmail.com>
2023-10-18 16:47:20 +0000

    PurposeのCountEncoding特徴量を追加 #8

--- run.py
+++ run.py
@@ -78,6 +78,9 @@ df["Prefecture_count"] = df["Prefecture"].map(df["Prefecture"].value_counts())
 # CityPlanningのCountEncoding特徴量を追加
 df["CityPlanning_count"] = df["CityPlanning"].map(df["CityPlanning"].value_counts())
 
+# PurposeのCountEncoding特徴量を追加
+df["Purpose_count"] = df["Purpose"].map(df["Purpose"].value_counts())
+
 target = "TradePrice"
 not_use_cols = [
     "row_id", "Prefecture", "Municipality", "DistrictName", "NearestStation",
