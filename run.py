ed98972b995d4f33967873dd5793e49ba5948fb6
takeko213 <takehara213@gmail.com>
2023-10-18 17:39:10 +0000

    NearestStationごとDirectionのカウント特徴量を追加 #65

--- run.py
+++ run.py
@@ -54,6 +54,9 @@ df["NearestStation_Frontage_rank"] = df.groupby("NearestStation")["Frontage"].ra
 
 df["NearestStation_Area_rank"] = df.groupby("NearestStation")["Area"].rank()
 
+# 追加: NearestStationごとDirectionのカウント特徴量
+df["NearestStation_Direction_count"] = df.groupby("NearestStation")["Direction"].transform("count")
+
 cat_cols = [
     "Type", "Region", "FloorPlan", "LandShape", "Structure",
     "Use", "Purpose", "Direction", "Classification", "CityPlanning",
@@ -72,10 +75,7 @@ df["Municipality_FloorAreaRatio_rank"] = df.groupby("Municipality")["FloorAreaRa
 
 df['Year-BuildingYear'] = df['Year'] - df['BuildingYear']
 
-# PrefectureのCountEncoding特徴量を追加
 df["Prefecture_count"] = df["Prefecture"].map(df["Prefecture"].value_counts())
-
-# CityPlanningのCountEncoding特徴量を追加
 df["CityPlanning_count"] = df["CityPlanning"].map(df["CityPlanning"].value_counts())
 
 target = "TradePrice"
