ffd7cb82bcc0096e8567f51ff37e50be651571c2
takeko213 <takehara213@gmail.com>
2023-10-19 02:21:32 +0000

    DistrictNameごとのFloorAreaRatioのrank特徴量を追加 #75

--- run.py
+++ run.py
@@ -56,6 +56,9 @@ df["DistinctName_count"] = df.groupby("DistrictName")["DistrictName"].transform(
 df["LandShape_count"] = df.groupby("NearestStation")["LandShape"].transform("count")
 df["NearestStation_Structure_count"] = df.groupby("NearestStation")["Structure"].transform("count")
 
+#add DistrictName_FloorAreaRatio_rank
+df["DistrictName_FloorAreaRatio_rank"] = df.groupby("DistrictName")["FloorAreaRatio"].rank()
+
 cat_cols = [
     "Type", "Region", "FloorPlan", "LandShape", "Structure",
     "Use", "Purpose", "Direction", "Classification", "CityPlanning",
